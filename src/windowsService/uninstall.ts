de58df020675a9e62c6478e25974b6f4e75642c2
Steve Cap <steven.cap@krisesystems.com>
2023-01-10 17:44:14 -0600

    format

--- src/windowsService/uninstall.ts
+++ src/windowsService/uninstall.ts
@@ -1,12 +1,13 @@
-import path from 'path';
-import { fileURLToPath } from 'url';
+import path from "path";
+import { fileURLToPath } from "url";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 import { Service } from "node-windows";
 import { createRequire } from "node:module";
 const require = createRequire(import.meta.url);
 let Config = require("../../config.json");
-let scriptPath = __dirname.substring(0, __dirname.indexOf("windowsService")) + "\\index.js"
+let scriptPath =
+  __dirname.substring(0, __dirname.indexOf("windowsService")) + "\\index.js";
 let svc = new Service({
   name: "StevesBot",
   description: "Steve's Bot",
